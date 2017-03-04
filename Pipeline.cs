f63a30b386b949fbc4b5081994dca8f7b2b7b8af
Jesse Weinstock <jweinstock@homesteadfunding.com>
2017-03-04 14:34:36 -0500

    built postPipelineQueryAsync

--- Pipeline.cs
+++ Pipeline.cs
@@ -1,7 +1,9 @@
 ﻿using EncompassREST.PipelineModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,8 +27,26 @@ namespace EncompassREST
 
         public async Task<string> postPipelineQueryAsync(Filter filter,List<SortOrderItem> sortItem,List<string> fields)
         {
+            var obj = new
+            {
+                filter = filter,
+                sortOrder = sortItem,
+                fields = fields
+            };
 
-            return "";
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, string.Format("loans/Pipeline"));
+            message.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF32, "applicaiotn/json");
+
+            var response = await _session.RESTClient.SendAsync(message);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new EncompassREST.Exceptions.RESTException("postPipelineQueryAsync", response);
+            }
+            
         }
 
     }
