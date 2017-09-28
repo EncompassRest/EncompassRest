8ecdd8192f4d274f1e167e43f7e05591d5b401d1
Jesse Weinstock <jweinstock@homesteadfunding.com>
2017-09-28 09:45:49 -0400

    Services API
    building out the new Services API module

---
+++ src/EncompassRest/Services/Options.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncompassRest.Services
+{
+    public sealed class Options
+    {
+        public bool DigiCert { get; set; }
+        public string RequestType { get; set; }
+        public string ReportOn { get; set; }
+        public string ReportType { get; set; }
+        public string CreditReportIdentifier { get; set; }
+        public string Note { get; set; }
+        public bool ExcludeZeroforImportLiabilities { get; set; }
+        public string CreditBureauExperian { get; set; }
+        public string CreditBureauEquifax { get; set; }
+        public string CreditBureauTransUnion { get; set; }
+    }
+}
---
+++ src/EncompassRest/Services/Services.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using EncompassRest.Utilities;
+using System.Collections;
+using System;
+
+namespace EncompassRest.Services
+{
+    public sealed class Services
+    {
+        private const string s_apiPath = "services/v1";
+
+        public EncompassRestClient Client { get; }
+
+        internal Services(EncompassRestClient client)
+        {
+            Client = client;
+        }
+
+        private async Task<T> OrderCreditServicesInternalAsync<T>(CreditResource resource,string partnerID, CancellationToken cancellationToken,string methodName, Func<HttpResponseMessage,Task<T>> func)
+        {
+            JsonStreamContent content;
+            content = JsonStreamContent.Create(resource);
+
+            using (var response = await Client.HttpClient.PostAsync($"{s_apiPath}/partners/{partnerID}/transactions", content, cancellationToken).ConfigureAwait(false))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw await RestException.CreateAsync(methodName, response).ConfigureAwait(false);
+                }
+                return await func(response).ConfigureAwait(false);
+            }
+        }
+
+    }
+}
