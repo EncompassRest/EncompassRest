441451e1f53f7a08b30c113217d4405632dc0eec
Tyler Brinkley <Tydude4Christ@hotmail.com>
2017-10-13 10:47:02 -0500

    Added webhook dirty checking (#46)

---
+++ src/EncompassRest.Tests/WebhookTests.cs
@@ -0,0 +1,24 @@
+﻿using EncompassRest.Utilities;
+using EncompassRest.Webhook;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EncompassRest.Tests
+{
+    [TestClass]
+    public class WebhookTests
+    {
+        [TestMethod]
+        public void WebhookSubscription_Serialization()
+        {
+            var subscription = new WebhookSubscription("https://google.com");
+            Assert.AreEqual(@"{""endpoint"":""https://google.com""}", subscription.ToJson());
+            subscription.Dirty = false;
+            Assert.AreEqual(@"{""endpoint"":""https://google.com""}", subscription.ToJson());
+            subscription.ClientId = "1234567890";
+            subscription.Events = new[] { "add", "update", "remove" };
+            Assert.AreEqual(@"{""endpoint"":""https://google.com"",""clientId"":""1234567890"",""events"":[""add"",""update"",""remove""]}", subscription.ToJson());
+            subscription.Dirty = false;
+            Assert.AreEqual(@"{""endpoint"":""https://google.com""}", subscription.ToJson());
+        }
+    }
+}
