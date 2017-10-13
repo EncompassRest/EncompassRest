b585462f9473caf12a9b7b70fc4a03d48ead611f
Jesse Weinstock <jweinstock@homesteadfunding.com>
2017-10-13 10:20:26 -0400

    get contact and borrowerContact

---
+++ src/EncompassRest/Contacts/BorrowerContacts.cs
@@ -0,0 +1,23 @@
+﻿using EncompassRest.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EncompassRest.Contacts
+{
+    public sealed class BorrowerContacts : Contacts
+    {
+        private static string s_apiPath = "encompass/v1/BorrowerContacts";
+        public BorrowerContacts(EncompassRestClient client) : base(client,s_apiPath){ }
+
+        public Task<BorrowerContact> GetBorrowerContactAsync(string contactId) => GetBorrowerContactAsync(contactId, CancellationToken.None);
+        public Task<BorrowerContact> GetBorrowerContactAsync(string contactId, CancellationToken cancellationToken) => 
+            GetContactInternalAsync(contactId, cancellationToken, response => response.Content.ReadAsAsync<BorrowerContact>());
+
+        public Task<string> GetBorrowerContactRawAsync(string contactId) => GetBorrowerContactRawAsync(contactId, CancellationToken.None);
+        public Task<string> GetBorrowerContactRawAsync(string contactId, CancellationToken cancellationToken) =>
+            GetContactRawInternalAsync(contactId, cancellationToken);
+    }
+}
--- src/EncompassRest/Contacts/Contacts.cs
+++ src/EncompassRest/Contacts/Contacts.cs
@@ -7,43 +7,33 @@ using System.Threading.Tasks;
 
 namespace EncompassRest.Contacts
 {
-    public enum ContactType
+    public abstract class Contacts
     {
-        Borrower,
-        Businesss
-    }
 
-    public sealed class Contacts
-    {
-        private const string s_apiPath = "encompass/v1/{ContactType}Contacts";
+        private string s_apiPath;
 
         #region Public Properties
-        public EncompassRestClient Client { get; }
+        internal EncompassRestClient Client { get; }
         #endregion
 
-        internal Contacts(EncompassRestClient client)
+        public Contacts(EncompassRestClient client, string apiPath)
         {
             Client = client;
+            s_apiPath = apiPath;
         }
 
-        virtual async Task<T> GetContactAsync<T>(string contactId, ContactType contactType, CancellationToken cancellationToken)
+        internal Task<string> GetContactRawInternalAsync(string contactId, CancellationToken cancellationToken) =>
+            GetContactInternalAsync(contactId, cancellationToken, response => response.Content.ReadAsStringAsync());
+
+        internal async Task<T> GetContactInternalAsync<T>(string contactId, CancellationToken cancellationToken, Func<HttpResponseMessage, Task<T>> func)
         {
             Preconditions.NotNullOrEmpty(contactId, nameof(contactId));
 
-            return GetContactInternalAsync(contactId, contactType, cancellationToken, resposne =>
-            {
-                var contact = new T(Client, contact);
-
-            });
-        }
-
-        private async Task<T> GetContactInternalAsync<T>(string contactId, ContactType contactType, CancellationToken cancellationToken, Func<HttpResponseMessage, Task<T>> func)
-        {
-            using (var response = await Client.HttpClient.GetAsync($"{s_apiPath.Replace("{ContactType}", contactType.ToString())}/{contactId}", cancellationToken).ConfigureAwait(false))
+            using (var response = await Client.HttpClient.GetAsync($"{s_apiPath}/{contactId}", cancellationToken).ConfigureAwait(false))
             {
                 if (!response.IsSuccessStatusCode)
                 {
-                    throw response.StatusCode == HttpStatusCode.NotFound ? await NotFoundException.CreateAsync($"{nameof(GetContactAsync)}/{contactId}", response).ConfigureAwait(false) : await RestException.CreateAsync(nameof(GetContactAsync), response).ConfigureAwait(false);
+                    throw response.StatusCode == HttpStatusCode.NotFound ? await NotFoundException.CreateAsync($"{nameof(T)}/{contactId}", response).ConfigureAwait(false) : await RestException.CreateAsync($"{nameof(T)}", response).ConfigureAwait(false);
                 }
                 return await func(response).ConfigureAwait(false);
             }
