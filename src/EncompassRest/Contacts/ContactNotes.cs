06b77999bf17d32b09c5ced2f69e241df78d568d
Jesse Weinstock <jweinstock@homesteadfunding.com>
2017-10-17 09:10:00 -0400

    overloads created

--- src/EncompassRest/Contacts/ContactNotes.cs
+++ src/EncompassRest/Contacts/ContactNotes.cs
@@ -24,6 +24,7 @@ namespace EncompassRest.Contacts
             _baseApiPath = baseApiPath;
         }
 
+        public Task<ContactNote> GetNoteAsync(string noteId) => GetNoteAsync(noteId, CancellationToken.None);
         public Task<ContactNote> GetNoteAsync(string noteId, CancellationToken cancellationToken)
         {
             Preconditions.NotNullOrEmpty(noteId, nameof(noteId));
@@ -35,13 +36,13 @@ namespace EncompassRest.Contacts
                 return contactNote;
             }));
         }
+        public Task<string> GetNoteRawAsync(string noteId) => GetNoteRawAsync(noteId, CancellationToken.None);
         public Task<string> GetNoteRawAsync(string noteId, CancellationToken cancellationToken)
         {
             Preconditions.NotNullOrEmpty(noteId, nameof(noteId));
 
             return GetNoteInternalAsync(noteId, cancellationToken, response => response.Content.ReadAsStringAsync());
         }
-
         private async Task<T> GetNoteInternalAsync<T>(string noteId, CancellationToken cancellationToken, Func<HttpResponseMessage, Task<T>> func)
         {
             using (var response = await Client.HttpClient.GetAsync($"{ _baseApiPath}/{ContactId}{s_apiPath}/{noteId}", cancellationToken).ConfigureAwait(false))
@@ -53,6 +54,8 @@ namespace EncompassRest.Contacts
                 return await func(response).ConfigureAwait(false);
             }
         }
+
+        public Task<List<ContactNote>> GetNotesAsync() => GetNotesAsync(CancellationToken.None);
         public Task<List<ContactNote>> GetNotesAsync(CancellationToken cancellationToken) => GetNotesInternalAsync(cancellationToken, async response =>
         {
             var contactNotes = await response.Content.ReadAsAsync<List<ContactNote>>().ConfigureAwait(false);
@@ -62,8 +65,8 @@ namespace EncompassRest.Contacts
             }
             return contactNotes;
         });
+        public Task<string> GetNotesRawAsync() => GetNotesRawAsync(CancellationToken.None);
         public Task<string> GetNotesRawAsync(CancellationToken cancellationToken) => GetNotesInternalAsync(cancellationToken, response => response.Content.ReadAsStringAsync());
-
         private async Task<T> GetNotesInternalAsync<T>(CancellationToken cancellationToken, Func<HttpResponseMessage, Task<T>> func)
         {
             using (var response = await Client.HttpClient.GetAsync($"{_baseApiPath}/{ContactId}{s_apiPath}", cancellationToken).ConfigureAwait(false))
@@ -75,6 +78,10 @@ namespace EncompassRest.Contacts
                 return await func(response).ConfigureAwait(false);
             }
         }
+
+        public Task<string> CreateNoteAsync(ContactNote contactNote) => CreateNoteAsync(contactNote, false, CancellationToken.None);
+        public Task<string> CreateNoteAsync(ContactNote contactNote, CancellationToken cancellationToken) => CreateNoteAsync(contactNote, false, cancellationToken);
+        public Task<string> CreateNoteAsync(ContactNote contactNote, bool populate) => CreateNoteAsync(contactNote, populate, CancellationToken.None);
         public Task<string> CreateNoteAsync(ContactNote contactNote, bool populate, CancellationToken cancellationToken)
         {
             Preconditions.NotNull(contactNote, nameof(contactNote));
@@ -89,6 +96,9 @@ namespace EncompassRest.Contacts
                 return Path.GetFileName(response.Headers.Location.OriginalString);
             });
         }
+        public Task<string> CreateNoteRawAsync(string contactNote) => CreateNoteRawAsync(contactNote, null, CancellationToken.None);
+        public Task<string> CreateNoteRawAsync(string contactNote, CancellationToken cancellationToken) => CreateNoteRawAsync(contactNote, null, cancellationToken);
+        public Task<string> CreateNoteRawAsync(string contactNote, string queryString) => CreateNoteRawAsync(contactNote, queryString, CancellationToken.None);
         public Task<string> CreateNoteRawAsync(string contactNote, string queryString, CancellationToken cancellationToken)
         {
             Preconditions.NotNullOrEmpty(contactNote, nameof(contactNote));
@@ -112,6 +122,9 @@ namespace EncompassRest.Contacts
             }
         }
 
+        public Task UpdateNoteAsync(ContactNote contactNote) => UpdateNoteAsync(contactNote, false, CancellationToken.None);
+        public Task UpdateNoteAsync(ContactNote contactNote, CancellationToken cancellationToken) => UpdateNoteAsync(contactNote, false, cancellationToken);
+        public Task UpdateNoteAsync(ContactNote contactNote, bool populate) => UpdateNoteAsync(contactNote, populate, CancellationToken.None);
         public Task UpdateNoteAsync(ContactNote contactNote, bool populate, CancellationToken cancellationToken)
         {
             Preconditions.NotNull(contactNote, nameof(contactNote));
@@ -126,6 +139,9 @@ namespace EncompassRest.Contacts
                 return string.Empty;
             });
         }
+        public Task<string> UpdateNoteRawAsync(string contactNoteId, string contactNote) => UpdateNoteRawAsync(contactNoteId, contactNote, null, CancellationToken.None);
+        public Task<string> UpdateNoteRawAsync(string contactNoteId, string contactNote, CancellationToken cancellationToken) => UpdateNoteRawAsync(contactNoteId, contactNote, null, cancellationToken);
+        public Task<string> UpdateNoteRawAsync(string contactNoteId, string contactNote, string queryString) => UpdateNoteRawAsync(contactNoteId, contactNote, queryString, CancellationToken.None);
         public Task<string> UpdateNoteRawAsync(string contactNoteId, string contactNote, string queryString, CancellationToken cancellationToken)
         {
             Preconditions.NotNullOrEmpty(contactNoteId, nameof(contactNoteId));
@@ -146,6 +162,7 @@ namespace EncompassRest.Contacts
             }
         }
 
+        public Task<bool> DeleteNoteAsync(string contactId) => DeleteNoteAsync(contactId, CancellationToken.None);
         public async Task<bool> DeleteNoteAsync(string contactNoteId, CancellationToken cancellationToken)
         {
             Preconditions.NotNullOrEmpty(contactNoteId, nameof(contactNoteId));
