5c5355f83a0bf1a48efc8d68cb8a75a2222fcb28
TylerBrinkley <Tydude4Christ@hotmail.com>
2018-02-05 10:53:42 -0600

    Added test

--- src/EncompassRest/Loans/MilestoneFreeRoles/LoanMilestoneFreeRole.cs
+++ src/EncompassRest/Loans/MilestoneFreeRoles/LoanMilestoneFreeRole.cs
@@ -4,8 +4,6 @@
     {
         private DirtyValue<string> _id;
         public string Id { get => _id; set => _id = value; }
-        private DirtyValue<string> _guid;
-        public string Guid { get => _guid; set => _guid = value; }
         private LoanAssociate _loanAssociate;
         public LoanAssociate LoanAssociate { get => _loanAssociate ?? (_loanAssociate = new LoanAssociate()); set => _loanAssociate = value; }
         internal override bool DirtyInternal
@@ -13,13 +11,11 @@
             get
             {
                 return _id.Dirty
-                    || _guid.Dirty
                     || _loanAssociate?.Dirty == true;
             }
             set
             {
                 _id.Dirty = value;
-                _guid.Dirty = value;
                 if (_loanAssociate != null) _loanAssociate.Dirty = value;
             }
         }
