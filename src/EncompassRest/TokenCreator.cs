748b141577de6112fd671c329e1d38ceb5877cd9
Tyler Brinkley <Tydude4Christ@hotmail.com>
2018-02-06 07:05:26 -0600

    Adds support for auto-retry on server-side timeouts. (#127)
    Adds support for specifying a timeout upon client creation through the ClientParameters parameter.
    
    Internalizes the RequestCancellationToken in TokenCreator so users don't need to remember to pass it on.
    
    Removes some obsolete methods.
    
    Includes some breaking changes.

--- src/EncompassRest/TokenCreator.cs
+++ src/EncompassRest/TokenCreator.cs
@@ -7,13 +7,19 @@ namespace EncompassRest
     {
         private readonly EncompassRestClient _client;
 
-        internal TokenCreator(EncompassRestClient client)
+        /// <summary>
+        /// The request <see cref="CancellationToken"/> is automatically applied to uses of this class.
+        /// </summary>
+        public CancellationToken RequestCancellationToken { get; }
+
+        internal TokenCreator(EncompassRestClient client, CancellationToken requestCancellationToken)
         {
             _client = client;
+            RequestCancellationToken = requestCancellationToken;
         }
 
-        public Task<string> FromUserCredentialsAsync(string userId, string password, CancellationToken cancellationToken = default) => _client.AccessToken.GetTokenFromUserCredentialsAsync(userId, password, nameof(FromUserCredentialsAsync), cancellationToken);
+        public Task<string> FromUserCredentialsAsync(string instanceId, string userId, string password, CancellationToken cancellationToken = default) => _client.AccessToken.GetTokenFromUserCredentialsAsync(instanceId, userId, password, nameof(FromUserCredentialsAsync), CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, RequestCancellationToken).Token);
 
-        public Task<string> FromAuthorizationCodeAsync(string redirectUri, string authorizationCode, CancellationToken cancellationToken = default) => _client.AccessToken.GetTokenFromAuthorizationCodeAsync(redirectUri, authorizationCode, nameof(FromAuthorizationCodeAsync), cancellationToken);
+        public Task<string> FromAuthorizationCodeAsync(string redirectUri, string authorizationCode, CancellationToken cancellationToken = default) => _client.AccessToken.GetTokenFromAuthorizationCodeAsync(redirectUri, authorizationCode, nameof(FromAuthorizationCodeAsync), CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, RequestCancellationToken).Token);
     }
 }
