611453687eac1fb8f6ad24493ee807be1332bb5d
TylerBrinkley <Tydude4Christ@hotmail.com>
2018-03-26 12:09:31 -0500

    Changed class to internal

--- src/EncompassRest/ModelPathSettings.cs
+++ src/EncompassRest/ModelPathSettings.cs
@@ -4,7 +4,7 @@ using System.Collections.ObjectModel;
 
 namespace EncompassRest
 {
-    public sealed class ModelPathSettings
+    internal sealed class ModelPathSettings
     {
         public int? IndexOffset { get; }
 
