8066d9b27e5fea07e8fcf0d66b0078d64da9576c
TylerBrinkley <Tydude4Christ@hotmail.com>
2018-07-23 13:06:40 -0500

    Added Webhook change event support and subscription filters support.
    Added better WebhookSubscription constructor overloads.
    
    Removed dirty checking for webhook subscriptions as the update method uses the PUT HTTP Verb instead of PATCH.

---
+++ src/EncompassRest/Webhook/WebhookFilters.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EncompassRest.Webhook
+{
+    public sealed class WebhookFilters : ExtensibleObject
+    {
+        private List<string> _attributes;
+        public IList<string> Attributes { get => _attributes ?? (_attributes = new List<string>()); set => _attributes = value != null ? new List<string>(value) : null; }
+
+        internal override bool DirtyInternal { get => true; set { } }
+    }
+}
