95e6202a7a9fc8f607466b4d7890993bdb219630
TylerBrinkley <Tydude4Christ@hotmail.com>
2019-09-06 14:49:05 -0500

    Added mocking test.

---
+++ src/EncompassRest.Tests/MockingTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using EncompassRest.Loans;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EncompassRest.Tests
+{
+    [TestClass]
+    public class MockingTests : TestBaseClass
+    {
+        [TestMethod]
+        public async Task MockTest()
+        {
+            var loanId = "abc";
+            var client = Mock.Of<IEncompassRestClient>(c => c.Loans.GetLoanAsync(loanId, default) == Task.FromResult(new Loan() { Id = loanId }));
+            var loan = await client.Loans.GetLoanAsync(loanId);
+            Assert.AreEqual(loanId, loan.Id);
+        }
+    }
+}
