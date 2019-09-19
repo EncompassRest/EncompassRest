1cf60952afd0a9ae9bd25e4fdef0bf634a5a99bc
Tyler Brinkley <Tydude4Christ@hotmail.com>
2019-09-19 15:19:08 -0500

    Added field reader metadata support (#290)

--- src/EncompassRest/Loans/FieldReader/LoanFieldReader.cs
+++ src/EncompassRest/Loans/FieldReader/LoanFieldReader.cs
@@ -31,6 +31,21 @@ namespace EncompassRest.Loans.FieldReader
         /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
         /// <returns></returns>
         Task<string> GetLoanFieldValuesRawAsync(string fieldIds, string queryString = null, CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Retrieve values for specific fields in a loan.
+        /// </summary>
+        /// <param name="includeMetadata">If set to true, the response will include the metadata for the fields specified, and returns details such as format, description, type, options and readOnly indicator.Default behavior is false, in which case, only the fieldId and value are returned for the fields in the request body.</param>
+        /// <param name="fieldIds">Field IDs of the values you want to retrieve from the loan.</param>
+        /// <returns></returns>
+        Task<List<LoanFieldValue>> GetLoanFieldValuesAsync(bool? includeMetadata, params string[] fieldIds);
+        /// <summary>
+        /// Retrieve values for specific fields in a loan.
+        /// </summary>
+        /// <param name="includeMetadata">If set to true, the response will include the metadata for the fields specified, and returns details such as format, description, type, options and readOnly indicator.Default behavior is false, in which case, only the fieldId and value are returned for the fields in the request body.</param>
+        /// <param name="fieldIds">Field IDs of the values you want to retrieve from the loan.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
+        /// <returns></returns>
+        Task<List<LoanFieldValue>> GetLoanFieldValuesAsync(bool? includeMetadata, IEnumerable<string> fieldIds, CancellationToken cancellationToken = default);
     }
 
     /// <summary>
@@ -48,7 +63,7 @@ namespace EncompassRest.Loans.FieldReader
         /// </summary>
         /// <param name="fieldIds">Field IDs of the values you want to retrieve from the loan.</param>
         /// <returns></returns>
-        public Task<List<LoanFieldValue>> GetLoanFieldValuesAsync(params string[] fieldIds) => GetLoanFieldValuesAsync(fieldIds, default);
+        public Task<List<LoanFieldValue>> GetLoanFieldValuesAsync(params string[] fieldIds) => GetLoanFieldValuesAsync(null, fieldIds, default);
 
         /// <summary>
         /// Retrieve values for specific fields in a loan.
@@ -56,11 +71,35 @@ namespace EncompassRest.Loans.FieldReader
         /// <param name="fieldIds">Field IDs of the values you want to retrieve from the loan.</param>
         /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
         /// <returns></returns>
-        public Task<List<LoanFieldValue>> GetLoanFieldValuesAsync(IEnumerable<string> fieldIds, CancellationToken cancellationToken = default)
+        public Task<List<LoanFieldValue>> GetLoanFieldValuesAsync(IEnumerable<string> fieldIds, CancellationToken cancellationToken = default) => GetLoanFieldValuesAsync(null, fieldIds, cancellationToken);
+
+        /// <summary>
+        /// Retrieve values for specific fields in a loan.
+        /// </summary>
+        /// <param name="includeMetadata">If set to true, the response will include the metadata for the fields specified, and returns details such as format, description, type, options and readOnly indicator.Default behavior is false, in which case, only the fieldId and value are returned for the fields in the request body.</param>
+        /// <param name="fieldIds">Field IDs of the values you want to retrieve from the loan.</param>
+        /// <returns></returns>
+        public Task<List<LoanFieldValue>> GetLoanFieldValuesAsync(bool? includeMetadata, params string[] fieldIds) => GetLoanFieldValuesAsync(includeMetadata, fieldIds, default);
+
+        /// <summary>
+        /// Retrieve values for specific fields in a loan.
+        /// </summary>
+        /// <param name="includeMetadata">If set to true, the response will include the metadata for the fields specified, and returns details such as format, description, type, options and readOnly indicator.Default behavior is false, in which case, only the fieldId and value are returned for the fields in the request body.</param>
+        /// <param name="fieldIds">Field IDs of the values you want to retrieve from the loan.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
+        /// <returns></returns>
+        public Task<List<LoanFieldValue>> GetLoanFieldValuesAsync(bool? includeMetadata, IEnumerable<string> fieldIds, CancellationToken cancellationToken = default)
         {
             Preconditions.NotNullOrEmpty(fieldIds, nameof(fieldIds));
 
-            return PostAsync<List<LoanFieldValue>>(null, null, JsonStreamContent.Create(fieldIds), nameof(GetLoanFieldValuesAsync), null, cancellationToken);
+            QueryParameters queryParameters = null;
+            if (includeMetadata.HasValue)
+            {
+                queryParameters = new QueryParameters();
+                queryParameters.Add("includeMetadata", includeMetadata.ToString().ToLower());
+            }
+
+            return PostAsync<List<LoanFieldValue>>(null, queryParameters?.ToString(), JsonStreamContent.Create(fieldIds), nameof(GetLoanFieldValuesAsync), null, cancellationToken);
         }
 
         /// <summary>
