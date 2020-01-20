3d2d02c27820030eb871cf3a9ad92fa9135c2434
Tyler Brinkley <Tydude4Christ@hotmail.com>
2020-01-20 11:12:34 -0600

    Add flag to allow setting read-only field values for local purposes. (#317)

--- src/EncompassRest/Loans/LoanFields.cs
+++ src/EncompassRest/Loans/LoanFields.cs
@@ -11,6 +11,12 @@ namespace EncompassRest.Loans
         private readonly Loan _loan;
 
         /// <summary>
+        /// Allows writing to read-only fields for local purposes such as unit tests. Sending an update request to Encompass with
+        /// a read-only field's value being set is likely to produce an error.
+        /// </summary>
+        public bool AllowWritesToReadOnlyFieldsLocally { get; set; }
+
+        /// <summary>
         /// Gets the loan field with the specified <paramref name="fieldId"/>.
         /// </summary>
         /// <param name="fieldId">The field id of the loan field to get. Borrower pair specified fields are supported, e.g. "4000#2".</param>
