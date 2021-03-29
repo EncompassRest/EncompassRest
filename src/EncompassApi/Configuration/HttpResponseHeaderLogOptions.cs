437e1b414bb7c029f923f2f6fc3e2f8ba0a5a321
Michael Seifi <michael.seifi@fairwaymc.com>
2021-03-29 14:01:40 -0500

    Httpresponse headers handlers

---
+++ src/EncompassApi/Configuration/HttpResponseHeaderLogOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncompassApi.Configuration
+{
+    public class HttpResponseHeaderLogOptions
+    {
+        public IEnumerable<string> Headers { get; set; }
+    }
+}
---
+++ src/EncompassApi/MessageHandlers/HttpResponseHandlers/LoggingResponseHeadersHandler.cs
@@ -0,0 +1,50 @@
+﻿using EncompassApi.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace EncompassApi.MessageHandlers
+{
+    public class LoggingResponseHeadersHandler : DelegatingHandler
+    {
+        private readonly IOptions<HttpResponseHeaderLogOptions> _options;
+        private readonly ILogger<LoggingResponseHeadersHandler> _logger;
+
+        public LoggingResponseHeadersHandler(
+            ILogger<LoggingResponseHeadersHandler> logger,
+            IOptions<Configuration.HttpResponseHeaderLogOptions> options)
+        {
+            _options = options;
+            _logger = logger;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var resp= await base.SendAsync(request, cancellationToken);
+            _logHeaders(resp);
+            return resp;
+        }
+
+        private void _logHeaders(HttpResponseMessage resp)
+        {
+            var headers = resp.Headers;
+            if (headers != null)
+            {
+                foreach (var key in _options?.Value.Headers)
+                {
+                    if (headers.TryGetValues(key, out IEnumerable<string> values))
+                    {
+                        _logger.LogDebug("Header {0} : {1}", key, values.FirstOrDefault());
+                    }
+                }
+            }
+        }
+
+    }
+}
