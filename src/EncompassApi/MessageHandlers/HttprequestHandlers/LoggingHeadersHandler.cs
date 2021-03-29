d4f355e0033620308e41d00cdb05d1d2b6c1e4e3
Michael Seifi <michael.seifi@fairwaymc.com>
2021-03-29 11:38:54 -0500

    Create the httprequestheader log file

---
+++ src/EncompassApi/MessageHandlers/HttprequestHandlers/LoggingHeadersHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EncompassApi.MessageHandlers
+{
+    public class LoggingHeadersHandler : DelegatingHandler
+    {
+        public LoggingHeadersHandler()
+        {
+
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var resp= await base.SendAsync(request, cancellationToken);
+            return resp;
+        }
+
+    }
+}
