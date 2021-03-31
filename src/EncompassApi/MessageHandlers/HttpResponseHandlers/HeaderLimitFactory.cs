24056b2051ce7ecf6361f9cd862e3f176a1d171a
Michael Seifi <michael.seifi@fairwaymc.com>
2021-03-31 09:32:04 -0500

    fix formatting

--- src/EncompassApi/MessageHandlers/HttpResponseHandlers/HeaderLimitFactory.cs
+++ src/EncompassApi/MessageHandlers/HttpResponseHandlers/HeaderLimitFactory.cs
@@ -53,12 +53,10 @@ namespace EncompassApi.MessageHandlers
             }
 
             return -1;
-           
         }
 
- 
-
     }
+
     public static class HeaderLimitFactoryExtensions
     {
         public static HeaderLimitFactory<THeader> Log<THeader, TLogger>(this HeaderLimitFactory<THeader> factory, THeader header, ILogger<TLogger> logger) where THeader : IHeaderLimit
