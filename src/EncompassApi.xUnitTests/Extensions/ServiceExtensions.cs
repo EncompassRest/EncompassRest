4ccaadcc107a9075a48dfd671fd29574c9ba29a1
Michael Seifi <michael.seifi@fairwaymc.com>
2021-04-06 12:33:54 -0500

    Add MockedEncompassHttpClient

---
+++ src/EncompassApi.xUnitTests/Extensions/ServiceExtensions.cs
@@ -0,0 +1,44 @@
+﻿using EncompassApi.Configuration;
+using EncompassApi.Extensions;
+using EncompassApi.xUnitTests.TestServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncompassApi.xUnitTests.Extensions
+{
+   public static class ServiceExtensions
+    {
+        public static IServiceCollection AddMockedEncompassHttpClient(this IServiceCollection services, Action<HttpClientOptions> config, Action<HttpClient> configureClient)
+        {
+            services.AddTransient<IMockedEncompassHttpClientService, MockedEncompassHttpClientService>();
+            return services;
+        }
+
+        public static IServiceCollection AddMockedEncompassHttpClient(this IServiceCollection services,  Action<HttpClient> configureClient)
+        {
+            services.AddTransient<IMockedEncompassHttpClientService, MockedEncompassHttpClientService>();
+            return services;
+        }
+
+        public static IServiceCollection AddMockedEncompassHttpClient(this IServiceCollection services, Action<HttpClientOptions> config)
+        {
+            services.AddTransient<IMockedEncompassHttpClientService, MockedEncompassHttpClientService>();
+            return services;
+        }
+
+        public static IServiceCollection AddMockedEncompassHttpClient(this IServiceCollection services)
+        {
+            services.AddTransient<IMockedEncompassHttpClientService, MockedEncompassHttpClientService>();
+            return services;
+        }
+
+
+    }
+}
---
+++ src/EncompassApi.xUnitTests/TestServices/IMockedEncompassHttpClientService.cs
@@ -0,0 +1,17 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace EncompassApi.xUnitTests.TestServices
+{
+    public interface IMockedEncompassHttpClientService
+    {
+        IHttpClientFactory HttpFactory { get; }
+        HttpClient MockedClient { get; }
+        EncompassApiService MockedEncompassClient { get; }
+        Mock<HttpMessageHandler> MockedHandler { get; }
+
+        void SetupResponseMessage(Action<HttpResponseMessage> action, KeyValuePair<string, string>? testHeader = null);
+    }
+}
