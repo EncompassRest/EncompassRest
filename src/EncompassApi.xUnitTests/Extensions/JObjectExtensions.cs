b2c49b39697bf5c6a03fe7895fc944cdea7c2967
Michael Seifi <michael.seifi@fairwaymc.com>
2021-04-06 12:34:50 -0500

    Create Jason JObject extensions

---
+++ src/EncompassApi.xUnitTests/Extensions/JObjectExtensions.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EncompassApi.xUnitTests.Extensions
+{
+    public static class JObjectExtensions
+    {
+        /// <summary>
+        /// Asserts that the jObjects are of certain type
+        /// </summary>
+        public static void ShouldBeOfType<TObject>(this JObject[] jObjects)
+        {
+            // THIS COULD BE REFINED MORE
+            // ....
+            foreach (var item in jObjects)
+            {
+                Assert.IsType<TObject>(item.ToObject<TObject>());
+            }
+        }
+
+        /// <summary>
+        /// Asserts that the two objects are equal based on an input logic
+        /// </summary>
+        /// <typeparam name="TObject"></typeparam>
+        /// <param name="isEqualFunc">Input logic to compare</param>
+        public static void AreEqual<TObject>(this JObject[] target, TObject[] source, Func<TObject, TObject, bool> isEqualFunc)
+        {
+            for (int i = 0; i < target.Length; i++)
+            {
+                Assert.True(isEqualFunc(source[i], target[i].ToObject<TObject>()));
+            }
+        }
+    }
+}
