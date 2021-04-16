53b92b260bbaa5db4e5229c8d3219810fcbbd8ec
Michael Seifi <michael.seifi@fairwaymc.com>
2021-04-16 12:08:38 -0500

    Update script to change private data in payloads

--- src/EncompassApi.xUnitTests/Payloads/CleanUpPayloads.cs
+++ src/EncompassApi.xUnitTests/Payloads/CleanUpPayloads.cs
@@ -10,33 +10,33 @@ namespace EncompassApi.xUnitTests.Payloads
 {
     public class CleanUpPayloads
     {
-
+        const string FILEPATH = "<Path to local respository>/src/EncompassApi.xUnitTests";
         [Theory]
         [WebhookSubscriptionFile(JsonFileTypes.Array)]
         public void WebhookSubscriptions(string methodName, params JObject[] objs)
         {
-            var subs = objs.Select(_ => _.ToObject<EncompassApi.Webhook.WebhookSubscription>());
+            var subs = objs.Select(_ => _.ToObject<EncompassApi.Webhook.WebhookSubscription>()).ToArray();
             foreach (var s in subs)
             {
                 s.Endpoint = Faker.Internet.SecureUrl();
                 s.InstanceId = Faker.Lorem.GetFirstWord();
             }
 
-            Helper.SaveJsonFile(methodName.Replace('_', '.'), Newtonsoft.Json.JsonConvert.SerializeObject(subs));
+            Helper.SaveJsonFile(methodName.Replace('_', '.'), FILEPATH, Newtonsoft.Json.JsonConvert.SerializeObject(subs));
         }
 
         [Theory]
         [WebhookSubscriptionFile(JsonFileTypes.Array, "Loan")]
         public void WebhookSubscriptions_Loan(string methodName, string resourceType, params JObject[] objs)
         {
-            var subs = objs.Select(_ => _.ToObject<EncompassApi.Webhook.WebhookSubscription>());
+            var subs = objs.Select(_ => _.ToObject<EncompassApi.Webhook.WebhookSubscription>()).ToArray();
             foreach (var s in subs)
             {
                 s.Endpoint = Faker.Internet.SecureUrl();
                 s.InstanceId = Faker.Lorem.GetFirstWord();
             }
 
-            Helper.SaveJsonFile(methodName.Replace('_', '.'), Newtonsoft.Json.JsonConvert.SerializeObject(subs));
+            Helper.SaveJsonFile(methodName.Replace('_', '.'), FILEPATH, Newtonsoft.Json.JsonConvert.SerializeObject(subs));
         }
     }
 }
--- src/EncompassApi.xUnitTests/Payloads/Helper.cs
+++ src/EncompassApi.xUnitTests/Payloads/Helper.cs
@@ -10,9 +10,9 @@ namespace EncompassApi.xUnitTests.Payloads
     public static class Helper
     {
 
-        public static void SaveJsonFile(string fileName,  string jsn)
+        public static void SaveJsonFile(string fileName, string filePath,  string jsn)
         {
-            var fullPath = $"Payloads/{fileName}.json";
+            var fullPath = $"{filePath}/Payloads/{fileName}.json";
 
             // Get the absolute path to the JSON file
             var path = Path.IsPathRooted(fullPath)
@@ -21,7 +21,7 @@ namespace EncompassApi.xUnitTests.Payloads
 
             //// Write that JSON to txt file,  
             //var read = System.IO.File.ReadAllText(path + "output.json");
-            System.IO.File.WriteAllText(path, jsn);
+            System.IO.File.WriteAllText(path, jsn, Encoding.UTF8);
         }
     }
 }
