36ba9bd4a30632c1ef1d01d809ac7d4d9163a9e6
Michael Seifi <michael.seifi@fairwaymc.com>
2021-04-16 14:31:52 -0500

    update payloads

--- src/EncompassApi.xUnitTests/Payloads/CleanUpPayloads.cs
+++ src/EncompassApi.xUnitTests/Payloads/CleanUpPayloads.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,31 +11,39 @@ namespace EncompassApi.xUnitTests.Payloads
 {
     public class CleanUpPayloads
     {
-        const string FILEPATH = "<Path to local respository>/src/EncompassApi.xUnitTests";
+        const string FILEPATH = "C:/Users/MichaelS/source/repos/RealMichaelSeifi/EncompassApi/src/EncompassApi.xUnitTests";
 
-        //[Theory]
-        //[WebhookSubscriptionFile(JsonFileTypes.Array)]
+        [Theory]
+        [WebhookSubscriptionFile(JsonFileTypes.Array)]
         public void WebhookSubscriptions(string methodName, params JObject[] objs)
         {
             var subs = objs.Select(_ => _.ToObject<EncompassApi.Webhook.WebhookSubscription>()).ToArray();
             foreach (var s in subs)
             {
-                s.Endpoint = Faker.Internet.SecureUrl();
-                s.InstanceId = Faker.Lorem.GetFirstWord();
+                s.Endpoint = "https://www.example.com/" + Faker.Name.First();
+                s.InstanceId = "be" + Faker.RandomNumber.Next(10000000, 99999999).ToString();
+                s.ClientId = Faker.RandomNumber.Next(10000000, 99999999).ToString();
+                s.Signingkey = Convert.ToBase64String((new System.Text.ASCIIEncoding()).GetBytes("sigingkey"));
+                s.ObjectUrn = $"urn:elli:webhook:{s.ClientId}:{s.InstanceId}:loan";
+
+
             }
 
             Helper.SaveJsonFile(methodName.Replace('_', '.'), FILEPATH, Newtonsoft.Json.JsonConvert.SerializeObject(subs));
         }
 
-        //[Theory]
-        //[WebhookSubscriptionFile(JsonFileTypes.Array, "Loan")]
+        [Theory]
+        [WebhookSubscriptionFile(JsonFileTypes.Array, "Loan")]
         public void WebhookSubscriptions_Loan(string methodName, string resourceType, params JObject[] objs)
         {
             var subs = objs.Select(_ => _.ToObject<EncompassApi.Webhook.WebhookSubscription>()).ToArray();
             foreach (var s in subs)
             {
-                s.Endpoint = Faker.Internet.SecureUrl();
-                s.InstanceId = Faker.Lorem.GetFirstWord();
+                s.Endpoint = "https://www.example.com/" + Faker.Name.First();
+                s.InstanceId = "be" + Faker.RandomNumber.Next(10000000, 99999999).ToString();
+                s.ClientId = Faker.RandomNumber.Next(10000000, 99999999).ToString();
+                s.Signingkey = Convert.ToBase64String((new System.Text.ASCIIEncoding()).GetBytes("sigingkey"));
+                s.ObjectUrn = $"urn:elli:webhook:{s.ClientId}:{s.InstanceId}:loan";
             }
 
             Helper.SaveJsonFile(methodName.Replace('_', '.'), FILEPATH, Newtonsoft.Json.JsonConvert.SerializeObject(subs));
